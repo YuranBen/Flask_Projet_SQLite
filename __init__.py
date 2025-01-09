c10911579b9dda99373aea67bc60aaf00adbb688
YuranBen <yuran.benbraham.pro@gmail.com>
2025-01-09 12:33:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,15 +49,6 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
-@app.route('/fiche_nom/<int:post_id>')
-def ReadBDD():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM clients;')
-    data = cursor.fetchall()
-    conn.close()
-    return render_template('read_data.html', data=data)
-
 
 @app.route('/consultation/')
 def ReadBDD():

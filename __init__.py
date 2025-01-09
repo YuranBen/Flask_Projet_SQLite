9d4d42514896c63c76f4ad0db705dfd8d29f398b
YuranBen <yuran.benbraham.pro@gmail.com>
2025-01-09 12:42:59 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -40,7 +40,7 @@ def authentification():
     return render_template('formulaire_authentification.html', error=False)
 
 
-@app.route('/fiche_nom/<int:post_id>')
+@app.route('/fiche_client/<int:post_id>')
 def Readfiche(post_id):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()

8b1bef66e98e0a518950f46dfa949cb60c0ebf1d
YuranBen <yuran.benbraham.pro@gmail.com>
2025-01-22 16:32:23 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -98,7 +98,7 @@ def rechercher_nom():
     return render_template('search_data_result.html')     
 
 
-@app.route('/fiche_nom', methods=['POST'])
+@app.route('/search_data', methods=['POST'])
 def recherche_client():
     nom = request.form['nom']
 
